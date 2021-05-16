c605cf864a6cc0eafae81717b5b2bc13a57761fe
John Mejia <jhondario.09@gmail.com>
2021-05-15 23:18:00 -0500

    Added new converter component in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+// Service
+import ratesApi from '../../services/converter_service';
+
+// Components
+import Converter from "../../components/Converter";
+
+// CSS
+import styles from './Home.module.scss';
 
 const Home = () => {
+    const [ratesData, setRatesData] = useState();
+
+    useEffect(() => {
+        const getRates = async () => {
+            const response = await ratesApi.get('/latest');
+            setRatesData(response.data);
+        };
+
+        getRates();
+    }, []);
+
     return (
-        <h1>Hello</h1>
+        <div className={styles.container}>
+            <header>
+                <h1 className={styles.title}>Exchange money</h1>
+            </header>
+            <Converter data={ratesData} />
+        </div>
     );
 };
 
